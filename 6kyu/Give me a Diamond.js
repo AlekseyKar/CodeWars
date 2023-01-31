e296a29e815f808c44c61f3adf1cfe74087c8dd7
AlekseyKar <alekseykar92@gmail.com>
2023-01-31 21:50:25 +0300

    feat/s-5: add 6, 7, 8kyu

---
+++ 6kyu/Give me a Diamond.js
@@ -0,0 +1,33 @@
+// You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+// Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+function diamond(n){
+  if (n < 0 || n % 2 === 0) {
+    return null;
+  }
+
+  let topArr = [];
+
+  for (let i = 1; i < n; i+=2) {
+    let spaces = " ".repeat((n - i) / 2);
+    let asterisks = "*".repeat(i);
+
+    topArr.push(spaces + asterisks);
+  }
+
+  return [...topArr, "*".repeat(n), ...topArr.reverse()].join("\n") + "\n" ;
+}
+
+
+// function diamond (n) {
+//   if (n <= 0 || n % 2 === 0) return null
+//   str = ''
+//   for (let i = 0; i < n; i++) { 
+//     let len = Math.abs((n-2*i-1)/2)
+//     str += ' '.repeat(len)
+//     str += '*'.repeat(n-2*len)
+//     str += '\n'
+//   }
+//   return str
+// }
---
+++ 8kyu/Grasshopper - Check for factor.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Эта функция должна проверять, является ли фактор .factorbase
+
+// Вернитесь, если это фактор или если это не так.truefalse
+
+function checkForFactor (base, factor) {
+  return base%factor===0
+}
