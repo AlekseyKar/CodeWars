5fb9f4b3dc462c7f5a3926bdf3c298990595bbb4
AleksKar <Alekseykar92@gmail.com>
2023-04-21 23:14:01 +0300

    feat/s-27: new 7kyu

--- 7kyu/Deodorant Evaporator.js
+++ 7kyu/Deodorant Evaporator.js
@@ -13,3 +13,9 @@ function evaporator(content, evap_per_day, threshold) {
 // Мы уменьшаем количество газа в емкости на процент потерь каждый день.
 // Мы продолжаем уменьшать количество газа в емкости каждый день до тех пор, пока количество газа не достигнет порогового значения.
 // Мы считаем количество дней, которые потребовались для того, чтобы количество газа в емкости достигло порогового значения
+
+// function evaporator(content, evap_per_day, threshold){ 
+//   threshold = threshold / 100
+//   evap_per_day = evap_per_day / 100
+//   return  Math.ceil(Math.log(threshold) / Math.log(1-evap_per_day))
+// }
