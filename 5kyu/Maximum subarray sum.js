1ba91ba1ae37e18fc9fd3e636f2e152791f56e5b
AleksKar <Alekseykar92@gmail.com>
2023-05-17 10:24:10 +0100

    feat/s-31: new 5, 6kyu

--- 5kyu/Maximum subarray sum.js
+++ 5kyu/Maximum subarray sum.js
@@ -4,10 +4,27 @@
 // // should be 6: [4, -1, 2, 1]
 // Простой случай, когда список состоит только из положительных чисел, а максимальная сумма является суммой всего массива. Если список состоит только из отрицательных чисел, вместо этого верните 0.
 
-let maxSequence = function(arr) {
-  let currSum = 0;
-  return arr.reduce(function(maxSum, number) {
-    currSum = Math.max(currSum + number, 0);
-    return currSum > maxSum? currSum : maxSum;
-  }, 0)
+// let maxSequence = function(arr) {
+//   let currSum = 0;
+//   return arr.reduce(function(maxSum, number) {
+//     currSum = Math.max(currSum + number, 0);
+//     return currSum > maxSum? currSum : maxSum;
+//   }, 0)
+// }
+
+function maxSequence(arr) {
+  let maxSum = 0;
+  let currentSum = 0;
+  
+  for(let i = 0; i < arr.length; i++) {
+    currentSum += arr[i];
+    if(currentSum < 0) {
+      currentSum = 0;
+    }
+    if(currentSum > maxSum) {
+      maxSum = currentSum;
+    }
+  }
+  
+  return maxSum;
 }
